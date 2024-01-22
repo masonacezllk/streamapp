7fdbc5072823b49641ebb9ba94a29eb283e6c01d
Masonacez Liu <545638818@qq.com>
2024-01-22 14:10:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+with st.container():
+   st.write("This is inside the container")
 
-with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+   # You can call any Streamlit command, including custom components:
+   st.bar_chart(np.random.randn(50, 3))
 
-with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+st.write("This is outside the container")
