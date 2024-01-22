24fd2e3b986d8e0e4e59118f940bb4caaa86011f
Masonacez Liu <545638818@qq.com>
2024-01-22 14:24:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
-container = st.container(border=True)
-container.write(st.checkbox("前排")
-st.write("This is outside the container")
+col1, col2, col3 = st.columns(3)
 
-# Now insert some more in the container
-container.write("This is inside too")
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
