dc538ba61d88d4c771a281818c3e09efd0aa8eec
Masonacez Liu <545638818@qq.com>
2024-01-22 14:20:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,15 @@ with st.container(border=True):
    check2 = st.checkbox('中间')
    check3 = st.checkbox('后排')
 
+with st.container(border=True):
+   check1 = st.checkbox('前排')
+   check2 = st.checkbox('中间')
+   check3 = st.checkbox('后排')
+st.write("This is outside the container")
+
 if check1:
     st.write('前排!')
 if check2:
     st.write('中间!')
 if check3:
     st.write('后排!')
-
-with st.container(border=True):
-   check1 = st.checkbox('前排')
-   check2 = st.checkbox('中间')
-   check3 = st.checkbox('后排')
-st.write("This is outside the container")
