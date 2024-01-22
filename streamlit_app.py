a09e50211fe23ba40ecd4ce0a482a1eefba479be
Masonacez Liu <545638818@qq.com>
2024-01-22 14:27:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+   st.header("前中后排定义")
+   with st.container():
+      st.checkbox("前排")
+      st.checkbox("中排")
+      st.checkbox("后排")
 
 with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+   st.header("位置定义")
+   with st.container():
+      st.checkbox("靠背")
+      st.checkbox("坐垫")
+      st.checkbox("地板")
 
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
