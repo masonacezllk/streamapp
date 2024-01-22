900a4334217213cb2f1dbd854aacd58f1bdc6e01
Masonacez Liu <545638818@qq.com>
2024-01-22 14:33:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,8 @@ with col2:
       st.checkbox("坐垫")
       st.checkbox("地板")
 
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
