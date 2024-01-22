3d38e15b2ca4adc87b0e2219a9bd496fe5ea8d5e
Masonacez Liu <545638818@qq.com>
2024-01-22 14:28:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,14 @@ col1, col2 = st.columns(2)
 
 with col1:
    st.header("前中后排定义")
-   with st.container():
+   with st.container(border=True):
       st.checkbox("前排")
       st.checkbox("中排")
       st.checkbox("后排")
 
 with col2:
    st.header("位置定义")
-   with st.container():
+   with st.container(border=True):
       st.checkbox("靠背")
       st.checkbox("坐垫")
       st.checkbox("地板")
